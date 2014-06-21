378626d99e25dab68500172784f67546d3452b06
Junjie Wu <junjiew@codeaurora.org>
2014-06-20 18:10:50 -0700

    arm: msm: Remove MSM_CPU_FREQ_SET_MIN_MAX related config
    MSM_CPU_FREQ_SET_MIN_MAX and related Kconfigs are deprecated. Purge
    them from Kconfig and qcom-cpufreq.
    
    Change-Id: I8ac786c155c7e235154b60c79f97d76ea15dace2
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -271,16 +271,8 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	if (is_clk && !cpu_clk[policy->cpu])
 		return 0;
 
-	if (cpufreq_frequency_table_cpuinfo(policy, table)) {
-#ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-		policy->cpuinfo.min_freq = CONFIG_MSM_CPU_FREQ_MIN;
-		policy->cpuinfo.max_freq = CONFIG_MSM_CPU_FREQ_MAX;
-#endif
-	}
-#ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
-	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
-#endif
+	if (cpufreq_frequency_table_cpuinfo(policy, table))
+		pr_err("cpufreq: failed to get policy min/max\n");
 
 	if (is_clk)
 		cur_freq = clk_get_rate(cpu_clk[policy->cpu])/1000;

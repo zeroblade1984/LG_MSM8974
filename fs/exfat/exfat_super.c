f3868bdcdfe4c9b387bdbd84ba53604b0c25d8f7
kairi_zeroblade <zeroblade1984@gmail.com>
2015-10-22 23:24:23 +0800

    EXFAT:Add some hacks from Dorimanx EXFAT repo
    Hacks for UTF8 and EXFAT module loading
    (https://github.com/dorimanx/exfat-nofuse/commit/d4a960696704ba678d09b9c5a96965ee42e7c6d4)

--- fs/exfat/exfat_super.c
+++ fs/exfat/exfat_super.c
@@ -2038,6 +2038,7 @@ enum {
 	Opt_err_panic,
 	Opt_err_ro,
 	Opt_err,
+	Opt_utf8_hack,
 #if EXFAT_CONFIG_DISCARD
 	Opt_discard,
 	Opt_nodiscard,
@@ -2062,6 +2063,7 @@ static const match_table_t exfat_tokens = {
 	{Opt_err_cont, "errors=continue"},
 	{Opt_err_panic, "errors=panic"},
 	{Opt_err_ro, "errors=remount-ro"},
+	{Opt_utf8_hack, "utf8"},
 #if EXFAT_CONFIG_DISCARD
 	{Opt_discard, "discard"},
 	{Opt_nodiscard, "nodiscard"},
@@ -2180,6 +2182,8 @@ static int parse_options(char *options, int silent, int *debug,
 		opts->readahead_kb = option * 1024;
 		break;
 #endif
+		case Opt_utf8_hack:
+			break;
 		default:
 			if (!silent) {
 				printk(KERN_ERR "[EXFAT] Unrecognized mount option %s or missing value\n", p);
@@ -2440,7 +2444,11 @@ static void exfat_debug_kill_sb(struct super_block *sb)
 
 static struct file_system_type exfat_fs_type = {
 	.owner       = THIS_MODULE,
+#if defined(CONFIG_MACH_LGE) || defined(CONFIG_HTC_BATT_CORE)
+	.name        = "texfat",
+#else
 	.name        = "exfat",
+#endif
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,37)
 	.get_sb      = exfat_get_sb,
 #else
@@ -2476,3 +2484,11 @@ void __exit exit_exfat_fs(void)
 //module_exit(exit_exfat_fs);
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("exFAT Filesystem Driver");
+#ifdef MODULE_ALIAS_FS
+#if defined(CONFIG_MACH_LGE) || defined(CONFIG_HTC_BATT_CORE)
+MODULE_ALIAS_FS("texfat");
+#else
+MODULE_ALIAS_FS("exfat");
+#endif
+#endif
